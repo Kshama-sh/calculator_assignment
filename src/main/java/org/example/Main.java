534efe27ebd095929a80b0a363e28631646d1be8
Kshama-sh <kshama.sh0903@gmail.com>
2025-02-10 12:20:30 +0530

    updated main function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,16 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        String expression = sc.next();
+        try{
+            double result=evaluate(expression);
+            System.out.println(result);
+        }
+        catch(Exception e){
+            System.out.println("invalid result");
+        }
     }
 }
